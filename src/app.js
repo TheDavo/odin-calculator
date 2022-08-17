28c8cdac3a249e8cbc53d4d3373f46c6024bb497
Davit Khudaverdyan <davkhuda@gmail.com>
2022-08-17 08:14:22 -0400

    Implement basic maths functionality, work on updating display correctly next

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 let currentResult = 0; //Actual number result
 let currentInput = document.getElementById('input'); // String input from calculator
+
 /* Initialize buttons with event listeners */
 const numbers = document.querySelectorAll('.buttons .number');
-const totalInput = document.querySelector('#total-input');
-const currentTotal = document.querySelector('#current-total');
+const totalInputDisplay = document.querySelector('#total-input');
+const currentTotalDisplay = document.querySelector('#current-total');
 
-let lhs = false; // left-hand-side
-let lhsValue = 0;
-// let rhs = false; // right-hand-side
-// let rhsValue = 0;
+let lhs = null;
+let rhs = null;
+let latestOperator = '';
+
+const operators = {
+  add: '+',
+  subtract: '-',
+  divide: '/',
+  multiply: 'x',
+  equal: '=',
+};
 
 for (let number of numbers) {
   number.addEventListener('click', (event) => {
@@ -22,39 +30,106 @@ for (let number of numbers) {
   });
 }
 
-function updateCalculator(operator) {
-  totalInput.innerText = totalInput.innerText.concat(
-    ' ',
-    currentInput.innerText,
-    `${operator}`
-  );
-  currentInput.innerText = '0';
-  currentTotal.innerText = currentResult;
-}
-
-/* Bind math functions to unique buttons */
 const buttonAdd = document.querySelector('#buttonAdd');
-buttonAdd.addEventListener('click', () => {
-  // if (lhs) {
-  //   currentResult = add(lhsValue, parseFloat(currentInput.innerText));
-  //   console.log(currentResult);
-  //   updateCalculator('+');
-  // } else {
-  //   lhs = true;
-  //   lhsValue = parseFloat(currentInput.innerText);
-  //   updateCalculator('+');
-  // }
+buttonAdd.addEventListener('click', function () {
+  latestOperator = 'add';
+  doMathAndUpdate('add');
 });
 
 const buttonSubtract = document.querySelector('#buttonSubtract');
-buttonSubtract.addEventListener('click', () => {});
+buttonSubtract.addEventListener('click', function () {
+  latestOperator = 'subtract';
+  doMathAndUpdate('subtract');
+});
 
-/* Bing non-math functions */
+const buttonMultiply = document.querySelector('#buttonMultiply');
+buttonMultiply.addEventListener('click', function () {
+  latestOperator = 'multiply';
+  doMathAndUpdate('multiply');
+});
+
+const buttonDivide = document.querySelector('#buttonDivide');
+buttonDivide.addEventListener('click', function () {
+  latestOperator = 'divide';
+  doMathAndUpdate('divide');
+});
+
+const buttonCompute = document.querySelector('#buttonCompute');
+buttonCompute.addEventListener('click', function () {
+  if (latestOperator) {
+    doMathAndUpdate(latestOperator);
+  }
+});
+
+const updateCalculator = function (operator) {
+  if (canDoMath()) {
+    totalInputDisplay.innerText = totalInputDisplay.innerText.concat(
+      ' ',
+      currentInput.innerText,
+      ' ',
+      `${operators[operator]}`
+    );
+  }
+  // else {
+  //   if (lhs != null && rhs == null) {
+  //     totalInputDisplay.innerText = ''.concat(
+  //       lhs,
+  //       ' ',
+  //       totalInputDisplay.innerText.replace(operators[latestOperator], operator)
+  //     );
+  //   }
+  // }
+  currentInput.innerText = '0';
+  currentTotalDisplay.innerText = currentResult;
+};
+
+const canDoMath = function () {
+  return lhs != null && rhs != null;
+};
+
+const doMath = function (operator, lhs, rhs) {
+  switch (operator) {
+    case 'add':
+      return add(lhs, rhs);
+    case 'subtract':
+      return subtract(lhs, rhs);
+    case 'multiply':
+      return multiply(lhs, rhs);
+    case 'divide':
+      return divide(lhs, rhs);
+  }
+};
+
+const doMathAndUpdate = function (operator) {
+  if (canDoMath()) {
+    currentResult = doMath(operator, lhs, rhs);
+    lhs = currentResult;
+    rhs = null;
+  } else {
+    const curIn = parseFloat(currentInput.innerText);
+    if (lhs == null) {
+      lhs = curIn;
+    } else if (rhs == null) {
+      if (curIn === 0) {
+        updateCalculator(operator);
+        return;
+      }
+      rhs = curIn;
+      currentResult = doMath(operator, lhs, rhs);
+      lhs = currentResult;
+      rhs = null;
+    }
+  }
+  updateCalculator(operator);
+};
+
+/* Bind non-math functions */
 const buttonAC = document.querySelector('#buttonAC');
 buttonAC.addEventListener('click', () => {
   currentResult = 0;
   currentInput.innerText = '0';
-  currentTotal.innerText = '0';
-  totalInput.innerText = '';
-  lhs = false;
+  currentTotalDisplay.innerText = '0';
+  totalInputDisplay.innerText = '';
+  lhs = null;
+  rhs = null;
 });
